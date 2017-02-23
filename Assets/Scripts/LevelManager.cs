474d2bafe958b59fb11c6f14a524322a5f5a2321
Megan Shaffer <mshaffer9@gatech.edu>
2017-02-23 17:16:24 -0500

    input manager, level manager, key and enemy classes

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public Camera elf;
+    public Camera dwarf;
+
+    public GameObject shirt;
+
+    public int health = 20;
+
+    public GameObject[] enemies;
+
+    public int keyCount = 10;
+
+    [HideInInspector]
+    public static LevelManager instance;
+
+    void Start()
+    {
+        instance = this;
+    }
+
+    void Update()
+    {
+        //lose state
+        if (health <= 0)
+        {
+            Debug.Log("you lost, gg scrub");
+        }
+
+        //win
+        if (keyCount == 0)
+        {
+            Debug.Log("winner winner chicken dinner bitches");
+        }
+    }
+}
