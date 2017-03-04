616022ced27b3f6d0d429214b5c27a16e8b4e555
Will Brewer <willbrewer182@gmail.com>
2017-03-04 15:43:00 -0500

    Inputs Done Baybee

--- Assets/Scripts/Players.cs
+++ Assets/Scripts/Players.cs
@@ -15,23 +15,34 @@ public class Players : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Debug.Log("(" + Input.GetAxis("Player1_LeftX") + "," + Input.GetAxis("Player1_LeftY") + ")");
+
+        Debug.Log(Input.GetAxis("Player1_X") + " " + Input.GetAxis("Player1_Y"));
+
+        Vector3 player1Move = new Vector3(Input.GetAxis("Player1_Y"), 0, 0);
+        Vector3 player1Rot = new Vector3(0, Input.GetAxis("Player1_X"), 0);
+
+        Vector3 player2Move = new Vector3(Input.GetAxis("Player2_Y"), 0, 0);
+        Vector3 player2Rot = new Vector3(0, Input.GetAxis("Player2_X"), 0);
 
 
+        //Transforms
+        transform.position = transform.position + camera1.transform.forward * player1Move.x * speed * Time.deltaTime;
+        transform.position = transform.position + camera2.transform.forward * player2Move.x * speed * Time.deltaTime;
+        //transform.Translate(player1Move * Time.deltaTime * speed);
+        transform.Rotate(player1Rot * Time.deltaTime * rotSpeed);
+        transform.Rotate(player2Rot * Time.deltaTime * rotSpeed);
+
+
+
+
+
+        /*
         Vector3 player1Move = new Vector3(Input.GetAxis("Player1_LeftX"), 0, Input.GetAxis("Player1_LeftY"));
         Vector3 player1Rot = new Vector3(0, Input.GetAxis("Player1_RightX"), 0);
 
         Vector3 player2Move = new Vector3(Input.GetAxis("Player2_LeftX"), 0, Input.GetAxis("Player2_LeftY"));
         Vector3 player2Rot = new Vector3(0, Input.GetAxis("Player2_RightX"), 0);
-
-        
-
-        Vector3 totalMove = player1Move + player2Move;
-
-
-        //Transforms
-        transform.Translate(totalMove * Time.deltaTime * speed);
-        transform.Rotate(player1Rot * Time.deltaTime * rotSpeed);
+        */
 
     }
 }
